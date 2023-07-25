a691ff3e512e8ad8a6901c4c48cc04ec2a5746b6
Parthiban <parthiban@bicsglobal.com>
2023-07-25 19:47:33 +0530

    New Files added

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
-const withPlugins = require("next-compose-plugins");
-const withImages = require("next-images");
-const withSass = require("@zeit/next-sass");
-const withCSS = require("@zeit/next-css");
-const webpack = require("webpack");
-const path = require("path");
+/**
+ * @type {import('next').NextConfig}
+ */
 
-module.exports = withPlugins([[withSass], [withImages], [withCSS]], {
-  webpack(config, options) {
-    config.resolve.modules.push(path.resolve("./"));
-    return config;
-  },
-});
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,96 +1,61 @@
-/*!
-
-=========================================================
-* NextJS Material Dashboard v1.1.0 based on Material Dashboard React v1.9.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-material-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-dashboard/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "next/app";
+import config from "@config/config.json";
+import theme from "@config/theme.json";
 import Head from "next/head";
-import Router from "next/router";
+import { useEffect, useState } from "react";
+import TagManager from "react-gtm-module";
+import "styles/style.scss";
 
-import PageChange from "components/PageChange/PageChange.js";
+const App = ({ Component, pageProps }) => {
+  // default theme setup
 
-import "assets/css/nextjs-material-dashboard.css?v=1.1.0";
+  // import google font css
+  const pf = theme.fonts.font_family.primary;
+  const sf = theme.fonts.font_family.secondary;
+  const [fontcss, setFontcss] = useState();
+  useEffect(() => {
+    fetch(
+      `https://fonts.googleapis.com/css2?family=${pf}${
+        sf ? "&family=" + sf : ""
+      }&display=swap`
+    ).then((res) => res.text().then((css) => setFontcss(css)));
+  }, [pf, sf]);
 
-Router.events.on("routeChangeStart", (url) => {
-  console.log(`Loading: ${url}`);
-  document.body.classList.add("body-page-transition");
-  ReactDOM.render(
-    <PageChange path={url} />,
-    document.getElementById("page-transition")
+  // google tag manager (gtm)
+  const tagManagerArgs = {
+    gtmId: config.params.tag_manager_id,
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      process.env.NODE_ENV === "production" &&
+        config.params.tag_manager_id &&
+        TagManager.initialize(tagManagerArgs);
+    }, 5000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <>
+      <Head>
+        {/* google font css */}
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <style
+          dangerouslySetInnerHTML={{
+            __html: `${fontcss}`,
+          }}
+        />
+        {/* responsive meta */}
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=5"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
   );
-});
-Router.events.on("routeChangeComplete", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
-});
-Router.events.on("routeChangeError", () => {
-  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
-  document.body.classList.remove("body-page-transition");
-});
+};
 
-export default class MyApp extends App {
-  componentDidMount() {
-    let comment = document.createComment(`
-
-=========================================================
-* * NextJS Material Dashboard v1.1.0 based on Material Dashboard React v1.9.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/nextjs-material-dashboard
-* Copyright 2021 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-dashboard/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-`);
-    document.insertBefore(comment, document.documentElement);
-  }
-  static async getInitialProps({ Component, router, ctx }) {
-    let pageProps = {};
-
-    if (Component.getInitialProps) {
-      pageProps = await Component.getInitialProps(ctx);
-    }
-
-    return { pageProps };
-  }
-  render() {
-    const { Component, pageProps } = this.props;
-
-    const Layout = Component.layout || (({ children }) => <>{children}</>);
-
-    return (
-      <React.Fragment>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          <title>NextJS Material Dashboard by Creative Tim</title>
-          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
-        </Head>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </React.Fragment>
-    );
-  }
-}
+export default App;
